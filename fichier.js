1b69ee3cc79c64c0dfa5b74d422f070b8af504d8
Hacene Baadji <hb21@live.fr>
2021-01-28 17:35:29 +0100

    separation du html et js

--- fichier.js
+++ fichier.js
@@ -1,9 +1,16 @@
-let x = 6;
-let y = 9;
 
-if(x>y){
-	alert('x contient une valeur superieur a 9');
+let x= 6 , y=9 ;
+		let a ="Hacene Bon goss";
+		let b = "gratte moi le cul";
+		let c = -20 , d = 40 ;
+		
 
-if (x<y){
-	alert( 'x contient une valeur inferieur a 9')
-}
+		if (x>y) { alert('x contient une valeur superieur a 9');} 
+		else {alert('x contient une valeur inferieur a 9');}
+
+	        
+        if (a!==b) {alert(a);} 
+        	else {alert(b);}
+
+        if (c<0) {alert("la valeur est :" + c);} 
+        	else {alert("la veur est :" + d);} 
